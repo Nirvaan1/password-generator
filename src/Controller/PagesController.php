d945ca3cab741f99a91e86a0c87f4cac22fef39a
Nirvaan <nirvaan.guilloux@madagence.com>
2022-03-01 00:18:54 +0100

    Add generatePassword and his template

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,15 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_pages')]
     public function index(): Response
     {
+
         return $this->render('pages/home.html.twig');
+    }
+
+    #[Route('/generate-password', name: 'app_generate_password')]
+    public function generatePassword(Request $request)
+    {
+        dump($request->query->all());
+        return $this->render('pages/password.html.twig');
+
     }
 }

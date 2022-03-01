8b964ccf0a4d8ef13c39040da5f3b455f5cf030e
Nirvaan <nirvaan.guilloux@madagence.com>
2022-03-01 23:21:22 +0100

    Generate-password

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -12,6 +12,10 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_pages')]
     public function index(): Response
     {
+        $characters = range('a', 'z');
+        $characters = array_merge($characters, range('A', 'Z'));
+        $characters = array_merge($characters, range(0, 9));
+        $characters = array_merge($characters, array('/','\\',':',';','!','@','#','$','%','^','*','(',')','_','+','=','|','{','}','[',']','"',"'",'<','>',',','?','~','`','&',' ','.'));
 
         return $this->render('pages/home.html.twig');
     }
@@ -19,8 +23,36 @@ class PagesController extends AbstractController
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request)
     {
-        dump($request->query->all());
-        return $this->render('pages/password.html.twig');
+        $length = $request->query->getInt('length');
+        $digits = $request->query->getBoolean('digits');
+        $uppercaseLetters = $request->query->getBoolean('uppercase_letters');
+        $specialCharacters = $request->query->getBoolean('special_characters');
+
+        $characters = range('a', 'z');
+        if ($uppercaseLetters){
+            $characters = array_merge($characters, range('A', 'Z'));
+        }
+        if ($digits){
+            $characters = array_merge($characters, range(0, 9));
+        }
+        if ($specialCharacters){
+            $characters = array_merge($characters, array('/','!','@','#','$','%','^','*','(',')','_','+','=','|','{','}','[',']','<','>',',','?','~','`','&',' ','.'));
+        }
+
+        $password = '';
+        $tab = count($characters) ;
+
+        for ($i = 0; $i < $length; $i++){
+            $password .=  $characters[mt_rand($i, $tab -1 ) ];
+        }
+
+
+        dump($password);
+
+
+        return $this->render('pages/password.html.twig', [
+            'password' => $password,
+        ]);
 
     }
 }

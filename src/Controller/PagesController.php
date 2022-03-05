f04c76c1a5fa42f2c61bd2d2f5714cbc7d2cb9f0
Nirvaan <nirvaan.guilloux@madagence.com>
2022-03-05 23:21:07 +0100

    Define password conf in parameters

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\PasswordGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,14 +14,23 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_pages')]
     public function index(): Response
     {
-        return $this->render('pages/home.html.twig');
+        return $this->render('pages/home.html.twig', [
+            'password_min_length' => $this->getParameter('app.password_min_length'),
+            'password_max_length' => $this->getParameter('app.password_max_length'),
+            'password_default_length' => $this->getParameter('app.password_default_length'),
+        ]);
     }
 
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request, PasswordGenerator $passwordGenerator): Response
     {
+        $lenght = max(
+            min($request->query->getInt('length'), $this->getParameter('app.password_max_length')),
+            $this->getParameter('app.password_min_length')
+        );
+
         $password = $passwordGenerator->generate(
-            length : $request->query->getInt('length'),
+            length : $lenght,
             digits : $request->query->getBoolean('digits'),
             uppercaseLetters : $request->query->getBoolean('uppercase_letters'),
             specialCharacters : $request->query->getBoolean('special_characters')
